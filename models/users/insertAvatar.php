d64739740e2c36d4fffc54be8dfd31e037c743f0
Anja Tomić <72461048+anjat99@users.noreply.github.com>
2020-10-09 20:44:54 +0200

    Delete insertAvatar.php

--- models/users/insertAvatar.php
+++
@@ -1,8 +0,0 @@
-<?php
-    ob_start();
-    if(isset($_POST['btnRegistracija'])){
-        //  echo "Kliknuto";
-        require_once "../../config/konekcija.php";
-    }else{
-        echo "NIje kliknuto na taster";
-   }
